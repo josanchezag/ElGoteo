a47ac72f1dea217ca8a0a1b9a56898c6ea2d4594
josanchezag <josanchezag@gmail.com>
2023-01-22 09:10:18 -0500

    First commit, creating the project structure folder

---
+++ src/main/java/com/elgotero/elgotero/ElgoteroApplication.java
@@ -0,0 +1,13 @@
+package com.elgotero.elgotero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElgoteroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElgoteroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elgotero/elgotero/ElgoteroApplicationTests.java
@@ -0,0 +1,10 @@
+package com.elgotero.elgotero;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElgoteroApplicationTests {
+
+
+
+}
