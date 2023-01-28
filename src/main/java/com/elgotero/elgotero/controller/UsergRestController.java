bc519b0911bc8f0b7f20a50ec4b3f88d43081b8e
josanchezag <josanchezag@gmail.com>
2023-01-28 00:30:58 -0500

    Creating Rest service to Client CRUDE

--- src/main/java/com/elgotero/elgotero/controller/UsergRestController.java
+++ src/main/java/com/elgotero/elgotero/controller/UsergRestController.java
@@ -1,37 +1,38 @@
 package com.elgotero.elgotero.controller;
 
-
 import com.elgotero.elgotero.model.Client;
-import com.elgotero.elgotero.repository.IClientRepository;
+import com.elgotero.elgotero.model.Userg;
+import com.elgotero.elgotero.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
-@RequestMapping("/Clients")
-public class ClientRestController {
+@RequestMapping("/Users")
+public class UsergRestController {
+
     @Autowired
-    IClientRepository repo;
+    IUserRepository repo;
 
     @GetMapping
-    public List<Client> getAll() {
+    public List<Userg> getAll() {
         return repo.findAll();
     }
 
     @PostMapping
-    public Client insertClient(@RequestBody Client p) {
+    public Userg insertClient(@RequestBody Userg p) {
         return repo.save(p);
     }
 
     @PutMapping
-    public Client modifyClient(@RequestBody Client p) {
+    public Userg modifyClient(@RequestBody Userg p) {
         return repo.save(p);
     }
 
     @DeleteMapping()
-    public void deleteClientbyId(@RequestParam(value = "id", required = false) Long id) {
+    public void deleteClientbyId(@RequestParam(value = "id", required = false) Integer id) {
         repo.deleteById(id);
     }
+
 }
